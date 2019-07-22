f750a465bd9e430ef2344f9e3c7f72ffd3c3a94d
banana <savannah.allsop@bet365.com>
2019-07-22 14:43:11 +0100

    rearranged the function order to match riak_core_console, added more functions from riak-admin - except for exoskeleskin functions, updated docs around the  functions for the specific functions

--- src/riak_stat.erl
+++ src/riak_stat.erl
@@ -12,34 +12,14 @@
 
 -include("riak_stat.hrl").
 
-%% Externally needed API
--export([
-    prefix/0,
-    set_priority/1,
-    get_stats/0
-]).
-
-%% Admin API
--export([
-    register/2,
-    get_app_stats/1,
-    get_stats_status/1,
-    get_stats_info/1,
-    get_value/1,
-    aggregate/2,
-    update/3,
-    unregister/4,
-    unregister/1
-]).
-
 %% Console API
 -export([
     show_stat_status/2,
     show_static_stats/1,
-    show_stat_info/1,
     disable_stat_0/1,
-    reset_stat/1,
-    change_stat_status/2
+    show_stat_info/1,
+    change_stat_status/2,
+    reset_stat/1
 ]).
 
 %% Profile API
@@ -50,42 +30,149 @@
     reset_stats_and_profile/0
 ]).
 
+%% Admin API
+-export([
+    prefix/0,
+    register/2,
+    get_stats/0,
+    get_app_stats/1,
+    get_stats_status/1,
+    get_stats_info/1,
+    get_value/1,
+    aggregate/2,
+    update/3,
+    unregister/4,
+    unregister/1
+]).
+
+%% Other riak_core_console API
+-export([
+    enable_metadata/1,
+    data_persist/2
+]).
+
+-define(STAT_PREFIX, stat_prefix).
+
 %%%===================================================================
-%%% Needed API
+%%% Console API
 %%%===================================================================
 %%%-------------------------------------------------------------------
 %%% @doc
-%%% API that is called from all or most of the other modules
+%%% Functions that are called from riak_core_console that are specific
+%%% for the "riak-admin stat ..." commands.
+%%% Are sent to the riak_stat_console module depending on the cmd.
 %%% @end
 %%%-------------------------------------------------------------------
 
--spec(prefix() -> value()).
-%% @doc standard prefix for all stats inside riak, all modules call
-%% into this function for the prefix.
-%% @end
-prefix() ->
-    app_helper:get_env(riak_stat, stat_prefix, riak).
-
--spec(set_priority(arg()) -> ok).
+-spec(show_stat_status(data(), status()) -> ok | print()).
 %% @doc
-%% Priority is an additional functionality that allows the metadata
-%% to be used or bypassed, in case of an error etc.. can be set in riak
-%% attach riak_stat:set_priority(metadata | exometer) or in the riak
-%% client "riak-admin stat set-priority <entry>"
+%% A call of riak-admin stat show-enabled | show-disabled <entry>
+%% points to this function, it will by default go to metadata unless it
+%% is changed to exometer with set_default(Def)...
+%% It just returns a status or statuses of the entry or entries
+%% provided.
 %% @end
-set_priority(Priority) when is_atom(Priority) ->
-    riak_stat_admin:set_priority(Priority);
-set_priority(Priority) when is_list(Priority) ->
-    set_priority(list_to_atom(Priority));
-set_priority(Priority) when is_binary(Priority) ->
-    set_priority(binary_to_existing_atom(Priority, latin1)).
+show_stat_status(Arg, Status) ->
+    riak_stat_console:show_stat(Arg, Status).
 
--spec(get_stats() -> stats()).
+-spec(show_static_stats(data()) -> ok | print()).
 %% @doc
-%% Pulls the list of stats out of the riak_stat_admin ets table
+%% A call from riak_core_console made through riak-admin of:
+%% riak-admin stat show-0 <entry> -> which returns the stats in
+%% exometer which have a value that is not updating.
+%% It will show which stats that are not being used.
+%%
+%% %% in exometer if it is not updating it will have a value of 0
+%% or []
 %% @end
-get_stats() ->
-    riak_stat_admin:get_stats().
+show_static_stats(Arg) ->
+    riak_stat_console:show_stat_0(Arg).
+
+-spec(disable_stat_0(data()) -> ok | print()).
+%% @doc
+%% like the function above it will find the stats in exometer that are not
+%% updating but will in turn disable them in both the metadata and in
+%% exometer so the change is persisted
+%% @end
+disable_stat_0(Arg) ->
+    riak_stat_console:disable_stat_0(Arg).
+
+-spec(show_stat_info(data()) -> ok | print()).
+%% @doc
+%% show the information that is kept in the metadata and in exometer for the stats
+%% given
+%% If the default is not metadata it will just return the information stored in
+%% exometer
+%% @end
+show_stat_info(Arg) ->
+    riak_stat_console:stat_info(Arg).
+
+-spec(change_stat_status(data(), status()) -> ok | print()).
+%% @doc
+%% change the status of the stat in metadata and exometer, unless the default
+%% is not metadata and is set to exometer then the data goes to exometer
+%% only.
+%% @end
+change_stat_status(Arg, ToStatus) ->
+    riak_stat_console:status_change(Arg, ToStatus).
+
+-spec(reset_stat(data()) -> ok | term()).
+%% @doc
+%% reset the stat in the metadata and in exometer, both the metadata
+%% and exometer keep history of the number of resets, except with the
+%% metadata the number of resets is persisted
+%% @end
+reset_stat(Arg) ->
+    riak_stat_console:reset_stat(Arg).
+
+
+%%%===================================================================
+%%% Profile API
+%%%===================================================================
+%%%-------------------------------------------------------------------
+%%% @doc
+%%% Specific functions for profiles in riak_stat, a layer of
+%%% functionality to save the current status of all stats to reload
+%%% later - allowing consistency in testing and laziness in stats.
+%%% @end
+%%%-------------------------------------------------------------------
+
+-spec(save_current_profile(data()) -> ok | error()).
+%% @doc
+%% Pull out the current stats status, and store the profile_name and
+%% list of stats into the metadata.
+%% All unregistered stats are stored as {status, unregistered}, but kept
+%% within the profile in case it becomes re_registered, it is defaulted to
+%% disabled.
+%% @end
+save_current_profile(Arg) ->
+    riak_stat_profiles:save_profile(Arg).
+
+-spec(load_profile(data()) -> ok | error()).
+%% @doc
+%% load a profile saved in the metadata, if the profile does not exist then
+%% {error, no_profile} is returned.
+%% @end
+load_profile(Arg) ->
+    riak_stat_profiles:load_profile(Arg).
+
+-spec(delete_profile(data()) -> ok | error()).
+%% @doc
+%% Deletes the profile in the metadata but does not reset the stats, that can
+%% be done manually with reset-profile, this just removes the
+%% snapshot of the stats from the metadata
+%% @end
+delete_profile(Arg) ->
+    riak_stat_profiles:delete_profile(Arg).
+
+-spec(reset_stats_and_profile() -> ok | error()).
+%% @doc
+%% Resets all disabled stats back to enabled. If the stat has a status of
+%% {status, unregistered} then the stat is left as unregistered or disabled.
+%% if it has been enabled since the profile was loaded then it will not be affected.
+%% @end
+reset_stats_and_profile() ->
+    riak_stat_profiles:reset_profile().
 
 
 %%%===================================================================
@@ -110,6 +197,14 @@ get_stats() ->
 %%% @end
 %%%-------------------------------------------------------------------
 
+-spec(prefix() -> value()).
+%% @doc
+%% standard prefix for all stats inside riak, all modules call
+%% into this function for the prefix.
+%% @end
+prefix() ->
+    riak_stat_config:get_env(?STAT_PREFIX, riak).
+
 -spec(register(app(), stats()) -> ok).
 %% @doc
 %% register the stats stored in the _stat modules in both the metadata
@@ -122,7 +217,7 @@ register(App, Stats) ->
 %% @doc
 %% pulls the list of stats out of riak_stat_admin for that app.
 %% @end
-get_app_stats(App) ->
+       get_app_stats(App) ->
     riak_stat_admin:read_stats(App).
 
 -spec(get_stats_status(app()) -> stats()).
@@ -154,129 +249,77 @@ aggregate(App, Stats) ->
 update(Name, IncrBy, Type) ->
     riak_stat_admin:update(Name, IncrBy, Type).
 
--spec(unregister(arg()) -> ok | print()).
+-spec(unregister(data()) -> ok | print()).
+%% @doc
+%% unregistering a stat puts tombstone in the metadata (if enabled)
+%% deletes the metric in exometer
+%% @end
 unregister({Mod, Idx, Type, App}) ->
     unregister(Mod, Idx, Type, App).
 
 unregister(Mod, Idx, Type, App) ->
     riak_stat_admin:unregister(prefix(), App ,Mod, Idx, Type).
 
+
 %%%===================================================================
-%%% Console API
+%%% Other API
 %%%===================================================================
 %%%-------------------------------------------------------------------
 %%% @doc
-%%% Functions that are called from riak_core_console that are specific
-%%% for the "riak-admin stat ..." commands.
-%%% Are sent to the riak_stat_console module depending on the cmd.
+%%% API that is called from riak_core_console specific for setting
+%%% options in the config or retrieving stats
 %%% @end
 %%%-------------------------------------------------------------------
 
--spec(show_stat_status(arg(), status()) -> ok | print()).
+-spec(get_stats() -> stats()).
 %% @doc
-%% A call of riak-admin stat show-enabled | show-disabled <entry>
-%% points to this function, it will by default go to metadata unless it
-%% is changed to exometer with set_default(Def)...
-%% It just returns a status or statuses of the entry or entries
-%% provided.
+%% Pulls all the stats out of metadata or exometer if metadata is disabled
 %% @end
-show_stat_status(Arg, Status) ->
-    riak_stat_console:show_stat(Arg, Status).
+get_stats() ->
+    riak_stat_coordinator:get_stats().
 
--spec(show_static_stats(arg()) -> ok | print()).
+-spec(enable_metadata(data()) -> ok).
 %% @doc
-%% A call from riak_core_console made through riak-admin of:
-%% riak-admin stat show-0 <entry> -> which returns the stats in
-%% exometer which have a value that is not updating.
-%% It will show which stats that are not being used.
+%% enabling the metadata allows the stats configuration and the stats values to
+%% be persisted, disabling the metadata returns riak to its original functionality
+%% of only using the exometer functions. Enabling and disabling the metadata occurs
+%% here, directing the stats and function work occurs in the riak_stat_coordinator
+%% @end
+enable_metadata(Arg) ->
+    Truth = ?IS_ENABLED(?META_ENABLED),
+    case riak_stat_admin:data_sanitise(Arg) of
+        Truth ->
+            case Truth of
+                true ->
+                    io:fwrite("Metadata already enabled~n");
+                false ->
+                    io:fwrite("Metadata already disabled~n")
+            end;
+        Bool when Bool == true; Bool == false ->
+            set_env(?META_ENABLED, Bool);
+        _ ->
+            io:fwrite("Wrong argument entered: ~p~n", [_])
+    end.
+
+-spec(data_persist(datapersist(), data()) -> ok | error()).
+%% @doc
+%% Data can be pulled out of exometer and stored into the metadata in
+%% order to be put into a DETS. It takes the argument Arg the same as
+%% any of the other console commands, "riak.riak_kv.**" etc...
 %%
-%% %% in exometer if it is not updating it will have a value of 0
-%% or []
+%% for data_persist(profile, Arg) the Arg will be the name of a profile
+%% stored in the metadata, specific to data persistence ot not.
 %% @end
-show_static_stats(Arg) ->
-    riak_stat_console:show_stat_0(Arg).
-
--spec(show_stat_info(arg()) -> ok | print()).
-%% @doc
-%% show the information that is kept in the metadata and in exometer for the stats
-%% given
-%% If the default is not metadata it will just return the information stored in
-%% exometer
-%% @end
-show_stat_info(Arg) ->
-    riak_stat_console:stat_info(Arg).
-
--spec(disable_stat_0(arg()) -> ok | print()).
-%% @doc
-%% like the function above it will find the stats in exometer that are not
-%% updating but will in turn disable them in both the metadata and in
-%% exometer so the change is persisted
-%% @end
-disable_stat_0(Arg) ->
-    riak_stat_console:disable_stat_0(Arg).
-
--spec(reset_stat(arg()) -> ok | term()).
-%% @doc
-%% reset the stat in the metadata and in exometer, both the metadata
-%% and exometer keep history of the number of resets, except with the
-%% metadata the number of resets is persisted
-%% @end
-reset_stat(Arg) ->
-    riak_stat_console:reset_stat(Arg).
-
--spec(change_stat_status(arg(), status()) -> ok | print()).
-%% @doc
-%% change the status of the stat in metadata and exometer, unless the default
-%% is not metadata and is set to exometer then the data goes to exometer
-%% only.
-%% @end
-change_stat_status(Arg, ToStatus) ->
-    riak_stat_console:status_change(Arg, ToStatus).
+data_persist(enabled, Arg) ->
+    riak_stat_admin:persist_data(Arg);
+data_persist(disabled, Arg) ->
+    riak_stat_admin:unpersist_data(Arg);
+data_persist(profile, Arg) ->
+    riak_stat_profiles:persist_profile_data(Arg).
 
 %%%===================================================================
-%%% Profile API
+%%% Helper Functions
 %%%===================================================================
-%%%-------------------------------------------------------------------
-%%% @doc
-%%% Specific functions for profiles in riak_stat, a layer of
-%%% functionality to save the current status of all stats to reload
-%%% later - allowing consistency in testing and laziness in stats.
-%%% @end
-%%%-------------------------------------------------------------------
 
--spec(save_current_profile(arg()) -> ok | error()).
-%% @doc
-%% Pull out the current stats status, and store the profile_name and
-%% list of stats into the metadata.
-%% All unregistered stats are stored as {status, unregistered}, but kept
-%% within the profile in case it becomes re_registered, it is defaulted to
-%% disabled.
-%% @end
-save_current_profile(Arg) ->
-    riak_stat_profiles:save_profile(Arg).
-
--spec(load_profile(arg()) -> ok | error()).
-%% @doc
-%% load a profile saved in the metadata, if the profile does not exist then
-%% {error, no_profile} is returned.
-%% @end
-load_profile(Arg) ->
-    riak_stat_profiles:load_profile(Arg).
-
--spec(delete_profile(arg()) -> ok | error()).
-%% @doc
-%% Deletes the profile in the metadata but does not reset the stats, that can
-%% be done manually with reset-profile, this just removes the
-%% snapshot of the stats from the metadata
-%% @end
-delete_profile(Arg) ->
-    riak_stat_profiles:delete_profile(Arg).
-
--spec(reset_stats_and_profile() -> ok | error()).
-%% @doc
-%% Resets all disabled stats back to enabled. If the stat has a status of
-%% {status, unregistered} then the stat is left as unregistered or disabled.
-%% if it has been enabled since the profile was loaded then it will not be affected.
-%% @end
-reset_stats_and_profile() ->
-    riak_stat_profiles:reset_profile().
+set_env(Arg, Value) ->
+    riak_stat_config:set_env(Arg, Value).
