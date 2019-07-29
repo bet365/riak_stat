db97df02361a8660f63b0ac091598a906479017c
banana <savannah.allsop@bet365.com>
2019-07-29 09:02:31 +0100

    cleaned up and added some more functions

--- src/riak_stat_config.erl
+++ src/riak_stat_config.erl
@@ -7,36 +7,34 @@
 
 -define(APP, riak_stat).
 
--export([get_env/1, get_env/2, set_env/2,
-  ensure_all/0, get_all_info/0, get_all/0]).
+-export([
+    get_env/1,
+    get_env/2,
+    set_env/2,
+    ensure_all/0,
+    get_all_info/0,
+    get_all/0
+]).
 
 get_env(Env) ->
-  {ok, P} = application:get_env(?APP, Env),
-  P.
+    {ok, P} = application:get_env(?APP, Env),
+    P.
 
 get_env(Env, Default) ->
-  {ok, P} = application:get_env(?APP, Env, Default),
-  P.
+    {ok, P} = application:get_env(?APP, Env, Default),
+    P.
 
 set_env(Env, Value) ->
-  application:set_env(?APP, Env, Value).
+    application:set_env(?APP, Env, Value).
 
 %% ---------------------------------------------------------------------
 
 ensure_all() ->
-  application:ensure_all_started(?APP).
+    application:ensure_all_started(?APP).
 
 get_all_info() ->
-  application:get_all_env(?APP).
+    application:get_all_env(?APP).
 
 get_all() ->
-  application:get_all_key(?APP).
+    application:get_all_key(?APP).
 
-
-%% todo
-
-% functions to check if a gen_server is alive,
-% a timer to wait a second before stats can register
-
-% If skeleskin is enabled definition
-% if so then set_env, it can be pulled out at the app and then the exoskeleskin will rise
