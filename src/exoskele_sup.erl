d94e1f6accb3f3eaf819fb57b51a7c0fe35fa1b2
banana <savannah.allsop@bet365.com>
2019-07-29 11:17:15 +0100

    added padding in to the supervisor and docs explaining the functions in the module, updated the ?CHILD definitions

--- src/exoskele_sup.erl
+++ src/exoskele_sup.erl
@@ -1,6 +1,7 @@
 %%%-------------------------------------------------------------------
 %%% @doc
-%%%
+%%% Setting up a gen_server to push stats to the endpoint provided or
+%%% to retrieve a list of stats in the K=V format
 %%% @end
 %%%-------------------------------------------------------------------
 -module(exoskele_sup).
@@ -25,16 +26,36 @@
 -define(RESTART, temporary).
 -define(SHUTDOWN, 6000).
 -define(TYPE, worker).
--define(CHILD(Name), {Name, {Name, start_link, []}, ?RESTART, ?SHUTDOWN, ?TYPE}).
--define(CHILD(Name, Arg), {Name, {Name, start_link, [Arg]}, ?RESTART, ?SHUTDOWN, ?TYPE}).
+-define(CHILD(Name), {Name, {Name, start_link, []}, ?RESTART, ?SHUTDOWN, ?TYPE, [Name]}).
+-define(CHILD(Name, Arg), {Name, {Name, start_link, [Arg]}, ?RESTART, ?SHUTDOWN, ?TYPE, [Name]}).
+
+%%%===================================================================
+%%% API functions
+%%%===================================================================
 
 start_link() ->
     supervisor:start_link({global, ?MODULE}, ?MODULE, []).
+
+%%%-------------------------------------------------------------------
+%%% @doc
+%%% Terminate the server(s) that are pushing stats to an endpoint
+%%% @end
+%%%-------------------------------------------------------------------
 
 terminate(Children) when is_list(Children) ->
     lists:foreach(fun(Child) ->
         stop_server(Child)
                   end, Children).
+
+stop_server(Mod) ->
+    supervisor:terminate_child(?SUPERVISOR, Mod), ok.
+
+%%%-------------------------------------------------------------------
+%%% @doc
+%%% Start up a udp or http gen_server to get stats from exometer and
+%%% send to an endpoint given.
+%%% @end
+%%%-------------------------------------------------------------------
 
 start_server(Name, Arg) ->
     Ref = ?CHILD(Name, Arg),
@@ -43,8 +64,20 @@ start_server(Name, Arg) ->
         {error, {already_started, Child}} -> Child
     end.
 
-stop_server(Mod) ->
-    supervisor:terminate_child(?SUPERVISOR, Mod), ok.
+
+%%%===================================================================
+%%% Supervisor callbacks
+%%%===================================================================
+%%%--------------------------------------------------------------------
+%%% @private
+%%% @doc
+%%% Whenever a supervisor is started using supervisor:start_link/[2,3],
+%%% this function is called by the new process to find out about
+%%% restart strategy, maximum restart frequency and child
+%%% specifications.
+%%%
+%%% @end
+%%%--------------------------------------------------------------------
 
 init([]) ->
     RestartStrategy = one_for_one,
@@ -52,19 +85,6 @@ init([]) ->
     MaxSecondsBetweenRestarts = 3600,
     SupFlags = {RestartStrategy, MaxRestarts, MaxSecondsBetweenRestarts},
 
-%%    HTTPChild =
-%%    case ?HTTP_ENABLED of
-%%        true -> ?CHILD(exoskele_http);
-%%        false -> []
-%%    end,
-%%
-%%    UDPChild =
-%%    case ?UDP_ENABLED of
-%%        true -> ?CHILD(exoskele_udp);
-%%        false -> []
-%%    end,
-
-%%    Children = lists:flatten([HTTPChild | UDPChild]),
 
     {ok, {SupFlags, []}}.
 
