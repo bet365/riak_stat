224f813bacba9c4a154d6bd1682070a805823e9b
banana <savannah.allsop@bet365.com>
2019-07-29 11:21:26 +0100

    changed timestamp() to call into riak_stat, removed old functions for parsing data and left the new improved sanitise_data_(Arg).

--- src/exoskele_data.erl
+++ src/exoskele_data.erl
@@ -1,6 +1,6 @@
 %%%-------------------------------------------------------------------
 %%% @doc
-%%%
+%%% Timestamps, formatting and sanitised data
 %%% @end
 %%%-------------------------------------------------------------------
 -module(exoskele_data).
@@ -12,10 +12,6 @@
   format_time/1,
   utc_milliseconds_time/0,
   exo_timestamp/0,
-  example/0,
-  parse_data/1,
-  get_port/1,
-  get_type/1,
   sanitise_data/1
 ]).
 
@@ -31,9 +27,9 @@
 %%--------------------------------------------------------------------
 -spec(format_time(erlang:timestamp()) -> string()).
 format_time({ _, _, MicroSeconds} = Now) ->
-  {{Year, Month, Day},{ Hour, Min, Sec}} = calendar:now_to_universal_time(Now),
-  lists:flatten(io_lib:format("~.4.0w-~.2.0w-~.2.0wT~.2.0w:~.2.0w:~.2.0w.~.3.0wZ",
-    [Year, Month, Day, Hour, Min, Sec, MicroSeconds div ?MILLISECONDS_MICROSECONDS_DIVISOR])).
+    {{Year, Month, Day},{ Hour, Min, Sec}} = calendar:now_to_universal_time(Now),
+    lists:flatten(io_lib:format("~.4.0w-~.2.0w-~.2.0wT~.2.0w:~.2.0w:~.2.0w.~.3.0wZ",
+        [Year, Month, Day, Hour, Min, Sec, MicroSeconds div ?MILLISECONDS_MICROSECONDS_DIVISOR])).
 
 %%--------------------------------------------------------------------
 %% @doc
@@ -42,8 +38,8 @@ format_time({ _, _, MicroSeconds} = Now) ->
 %%--------------------------------------------------------------------
 -spec(utc_milliseconds_time() -> term()).
 utc_milliseconds_time() ->
-  {Mega, Sec, Micro} = os:timestamp(),
-  (Mega * ?MEGASECS_MILLISECOND_DIVISOR) + (Sec * ?SECS_MILLISECOND_DIVISOR) + (Micro div ?MILLISECONDS_MICROSECONDS_DIVISOR).
+    {Mega, Sec, Micro} = os:timestamp(),
+    (Mega * ?MEGASECS_MILLISECOND_DIVISOR) + (Sec * ?SECS_MILLISECOND_DIVISOR) + (Micro div ?MILLISECONDS_MICROSECONDS_DIVISOR).
 
 %%--------------------------------------------------------------------
 %% @doc
@@ -52,7 +48,7 @@ utc_milliseconds_time() ->
 %%--------------------------------------------------------------------
 -spec(exo_timestamp() -> term()).
 exo_timestamp() ->
-  exometer_util:timestamp().
+    riak_stat:timestamp().
 
 %%--------------------------------------------------------------------
 %% @doc
@@ -62,14 +58,14 @@ exo_timestamp() ->
 %%--------------------------------------------------------------------
 -spec(sanitise_data(term()) -> sanitised_data()).
 sanitise_data(Arg) ->
-  List = break_up(Arg),
-  sanitise_data_(List).
+    List = break_up(Arg),
+    sanitise_data_(List).
 
 break_up(Arg) ->
-  re:split(Arg, "\\s", []).
+   re:split(Arg, "\\s", []).
 
 sanitise_data_(Arg) ->
-  sanitise_data_(Arg, undefined, ?MONITOR_STATS_PORT, ?INSTANCE, ?MONITOR_SERVER).
+    sanitise_data_(Arg, undefined, ?MONITOR_STATS_PORT, ?INSTANCE, ?MONITOR_SERVER).
 sanitise_data_([<<"protocol=", P/binary>> | Rest], _Protocol, Port, Instance, Sip) ->
     NewProtocol =
         case P of
@@ -99,66 +95,4 @@ sanitise_data_([<<"sip=", S/binary>> | Rest], Protocol, Port, Instance, Sip) ->
         end,
     sanitise_data_(Rest, Protocol, Port, Instance, NewIP);
 sanitise_data_([], Protocol, Port, Instance, Sip) ->
-  {Protocol, Port, Instance, Sip}.
-
-
-
-
-
-
-example() ->
-  List = re:split([<<"protocol=udp port=8080 instance=hellothere sip=127.0.0.1">>], "\\s"),
-%%  {{Protocol, Port, Instance, Sip},Extra}=
-  lists:map(fun
-                (<<"protocol=", P/binary>>) ->
-                  {protocol, P};
-                (<<"port=", P/binary>>) ->
-                  {port,P};
-                (<<"instance=", I/binary>>) ->
-                  {instance,I};
-                (<<"sip=", S/binary>>) ->
-                  {servr_ip,S}
-            end, List).
-
-
-
-parse_data([]) ->
-  0;
-parse_data(Data) when is_list(Data)->
-  lists:map(fun(P) ->
-    parse_data_(P)
-            end, Data);
-parse_data(Data) when is_integer(Data) ->
-  Data.
-
-parse_data_(Data) when is_binary(Data) ->
-  try binary_to_integer(Data)
-  catch error:_ -> parse_data_(binary_to_list(Data))
-  end;
-parse_data_(Data) when is_list(Data) ->
-  try list_to_integer(Data)
-  catch error:_ -> lager:error("Port is not an integer~n")
-  end.
-
-
-get_port(Port) when is_list(Port) == false andalso is_binary(Port) ->
-  get_port([Port]);
-get_port([<<"port=", P/binary>> | Rest]) ->
-  case P of
-    Po when is_binary(Po) ->
-      try binary_to_integer(Po)
-      catch error: _ -> Rest
-      end;
-    _ ->
-      Rest
-  end.
-
-
-get_type(Type) when is_list(Type) == false andalso is_binary(Type) ->
-  get_type([Type]);
-get_type([<<"type=", T/binary>> | Rest]) ->
-  case T of
-    <<"udp">> -> udp;
-    <<"http">> -> http;
-    _ -> Rest
-  end.
+    {Protocol, Port, Instance, Sip}.
