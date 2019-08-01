c50298d531921020dcdd75d277fa16df3071e4cd
banana <savannah.allsop@bet365.com>
2019-08-01 09:48:49 +0100

    added macros for exoskele_udp, added types fro jsonprops and serviceid() etc, and inserted default values for the gen_udp:open options

--- src/exoskeleskin.hrl
+++ src/exoskeleskin.hrl
@@ -6,32 +6,60 @@
 
 -define(APP, exoskeleskin).
 
--define(STATS_UPDATE_INTERVAL, riak_stat_config:get_env(exoskeleskin_stats_update_interval, 1000)).
--define(REFRESH_INTERVAL,      riak_stat_config:get_env(exoskeleskin_ip_refresh_interval, 30000)).
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
--define(SPIRAL_TIME_SPAN,      riak_stat_config:get_env(exoskeleskin_stats_spiral_time_span, 1000)).
--define(HISTOGRAM_TIME_SPAN,   riak_stat_config:get_env(exoskeleskin_stats_histogram_time_span, 1000)).
-
-%% OPTIONAL ENVIRONMENT VARIABLES
-
--define(EXCLUDED_DATAPOINTS,   riak_stat_config:get_env(exoskeleskin_excluded_datapoints, [ms_since_reset])).
--define(STATS_LISTEN_PORT,     riak_stat_config:get_env(stats_listen_port, 9000)).
--define(EXOSKELETABLE,         exoskeleskin_state).
+%% MACROS for exoskele_udp
 
 -define(INSTANCE,              riak_stat_config:get_env(instance)).
 -define(MONITOR_SERVER,        riak_stat_config:get_env(monitor_server)).
 -define(MONITOR_LATENCY_PORT,  riak_stat_config:get_env(monitor_latency_port)).
 -define(MONITOR_STATS_PORT,    riak_stat_config:get_env(monitor_stats_port)).
 
--type protocol()          :: udp | http.
+-define(EXCLUDED_DATAPOINTS,   riak_stat_config:get_env(exoskeleskin_excluded_datapoints, [ms_since_reset])).
+-define(STATS_LISTEN_PORT,     riak_stat_config:get_env(stats_listen_port, 9000)).
+-define(EXOSKELETABLE,         exoskeleskin_state).
+-define(UDP_KEY,               udp_socket).
+
+-define(STATS_UPDATE_INTERVAL, riak_stat_config:get_env(exoskeleskin_stats_update_interval, 1000)).
+-define(REFRESH_INTERVAL,      riak_stat_config:get_env(exoskeleskin_ip_refresh_interval, 30000)).
+
+-define(SPIRAL_TIME_SPAN,      riak_stat_config:get_env(exoskeleskin_stats_spiral_time_span, 1000)).
+-define(HISTOGRAM_TIME_SPAN,   riak_stat_config:get_env(exoskeleskin_stats_histogram_time_span, 1000)).
+
+-define(UDP_OPEN_PORT,         0).
+-define(UDP_OPEN_BUFFER,       {buffer, 100*1024*1024}).
+-define(UDP_OPEN_SNDBUFF,      {sndbuf, 5*1024*1024}).
+-define(UDP_OPEN_ACTIVE,       {active,false}).
+
+
+%% OPTIONAL ENVIRONMENT VARIABLES
+
+
+
+
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+%% types for exoskele_udp
+
+-type jsonprops()         :: [{atom(), any()}].
+-type serviceid()         :: string() | binary().
+-type correlationid()     :: string() | binary().
+
 -type socket()            :: inet:socket().
--type latency_port()      :: inet:port_number().
--type instance()          :: string().
--type server_ip()         :: inet:ip4_address().
 -type server()            :: inet:ip4_address().
+-type latency_port()      :: inet:port_number().
+-type server_ip()         :: inet:ip4_address().
+-type stats_port()        :: inet:port_number().
 -type hostname()          :: inet:hostname().
+-type instance()          :: string().
+
+
+
+-type protocol()          :: udp | http.
 
 -type sanitised_data()     :: {protocol(), socket(), latency_port(), port(),
                               instance(), server(), server_ip()}.
 -type reason()        :: any().
 -type error()         :: {error, reason()}.
+
