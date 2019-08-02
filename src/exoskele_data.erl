1bcb0dd37d3fc363f35b174bd2c50b8f68b34394
banana <savannah.allsop@bet365.com>
2019-08-02 11:31:16 +0100

    Moved sanitised_data to the top, removed updating functions, commented out execution time.

--- src/exoskele_data.erl
+++ src/exoskele_data.erl
@@ -12,96 +12,81 @@
   format_time/1,
   utc_milliseconds_time/0,
   exo_timestamp/0,
-  sanitise_data/1,
-
-  execution_time/2,
-  update_counter/2,
-  update_gauge/2,
-  update_histogram/2,
-  update_spiral/2,
-  update_or_create/4
+  sanitise_data/1
 ]).
 
 -define(MEGASECS_MILLISECOND_DIVISOR,1000000000).
 -define(SECS_MILLISECOND_DIVISOR,1000).
 -define(MILLISECONDS_MICROSECONDS_DIVISOR,1000).
 
+
 %%--------------------------------------------------------------------
+-spec(sanitise_data(arg()) -> sanitised_data()).
+%% @doc
+%% Sanitise the data coming in, into the necessary arguments for setting
+%% up an endpoint
+%% @end
+sanitise_data(Arg) ->
+  [Opts | Stats] = break_up(Arg, "/"),
+  List = break_up(Opts, "\\s"),
+  NewStats =
+    case Stats of
+      [] -> ['_'];
+      Data -> Data
+    end,
+  {sanitise_data_(List), NewStats}.
+
+break_up(Arg, Str) ->
+  re:split(Arg, Str, []).
+
+sanitise_data_(Arg) ->
+  sanitise_data_(Arg, ?MONITOR_STATS_PORT, ?INSTANCE, ?MONITOR_SERVER).
+sanitise_data_([<<"port=", Po/binary>> | Rest], Port, Instance, Sip) ->
+  NewPort =
+    case binary_to_integer(Po) of
+      {error, _reason} -> Port;
+      Int -> Int
+    end,
+  sanitise_data_(Rest, NewPort, Instance, Sip);
+sanitise_data_([<<"instance=", I/binary>> | Rest], Port, _Instance, Sip) ->
+  NewInstance = binary_to_list(I),
+  sanitise_data_(Rest, Port, NewInstance, Sip);
+sanitise_data_([<<"sip=", S/binary>> | Rest], Port, Instance, _Sip) ->
+  NewIP = re:split(S, "\\s", [{return, list}]),
+  sanitise_data_(Rest, Port, Instance, NewIP);
+sanitise_data_([], Port, Instance, Sip) ->
+  {Port, Instance, Sip}.
+
+
+
+%%--------------------------------------------------------------------
+-spec(format_time(erlang:timestamp()) -> string()).
 %% @doc
 %% For a given timestamp, returns the string representation in the following
 %% format, YYYY.MM.ddTHH:mm:ss.SSSZ the time is in UTC.
 %% @end
-%%--------------------------------------------------------------------
--spec(format_time(erlang:timestamp()) -> string()).
 format_time({ _, _, MicroSeconds} = Now) ->
     {{Year, Month, Day},{ Hour, Min, Sec}} = calendar:now_to_universal_time(Now),
     lists:flatten(io_lib:format("~.4.0w-~.2.0w-~.2.0wT~.2.0w:~.2.0w:~.2.0w.~.3.0wZ",
         [Year, Month, Day, Hour, Min, Sec, MicroSeconds div ?MILLISECONDS_MICROSECONDS_DIVISOR])).
 
 %%--------------------------------------------------------------------
+-spec(utc_milliseconds_time() -> term()).
 %% @doc
 %% Gets the utc time in milliseconds.
 %% @end
-%%--------------------------------------------------------------------
--spec(utc_milliseconds_time() -> term()).
 utc_milliseconds_time() ->
     {Mega, Sec, Micro} = os:timestamp(),
     (Mega * ?MEGASECS_MILLISECOND_DIVISOR) + (Sec * ?SECS_MILLISECOND_DIVISOR) + (Micro div ?MILLISECONDS_MICROSECONDS_DIVISOR).
 
 %%--------------------------------------------------------------------
+-spec(exo_timestamp() -> term()).
 %% @doc
 %% timestamp format used with exometer_histogram and exometer_slide
 %% @end
-%%--------------------------------------------------------------------
--spec(exo_timestamp() -> term()).
 exo_timestamp() ->
     riak_stat:timestamp().
 
-%%--------------------------------------------------------------------
-%% @doc
-%% Sanitise the data coming in, into the necessary arguments for setting
-%% up an endpoint
-%% @end
-%%--------------------------------------------------------------------
--spec(sanitise_data(term()) -> sanitised_data()).
-sanitise_data(Arg) ->
-    [Opts | Stats] = break_up(Arg, "/"),
-    List = break_up(Opts, "\\s"),
-    NewStats =
-      case Stats of
-        [] -> ['_'];
-        Data -> Data
-      end,
-    {sanitise_data_(List), NewStats}.
-
-break_up(Arg, Str) ->
-   re:split(Arg, Str, []).
-
-sanitise_data_(Arg) ->
-    sanitise_data_(Arg, undefined, ?MONITOR_STATS_PORT, ?INSTANCE, ?MONITOR_SERVER).
-sanitise_data_([<<"protocol=", P/binary>> | Rest], _Protocol, Port, Instance, Sip) ->
-    NewProtocol =
-        case P of
-          <<"udp">> -> P;
-          <<"http">> -> P;
-          _ -> undefined
-        end,
-    sanitise_data_(Rest, NewProtocol, Port, Instance, Sip);
-sanitise_data_([<<"port=", Po/binary>> | Rest], Protocol, Port, Instance, Sip) ->
-    NewPort =
-        case binary_to_integer(Po) of
-          {error, _reason} -> Port;
-          Int -> Int
-        end,
-    sanitise_data_(Rest, Protocol, NewPort, Instance, Sip);
-sanitise_data_([<<"instance=", I/binary>> | Rest], Protocol, Port, _Instance, Sip) ->
-    NewInstance = binary_to_list(I),
-    sanitise_data_(Rest, Protocol, Port, NewInstance, Sip);
-sanitise_data_([<<"sip=", S/binary>> | Rest], Protocol, Port, Instance, _Sip) ->
-    NewIP = re:split(S, "\\s", [{return, list}]),
-    sanitise_data_(Rest, Protocol, Port, Instance, NewIP);
-sanitise_data_([], Protocol, Port, Instance, Sip) ->
-    {Protocol, Port, Instance, Sip}.
 
 
 
@@ -109,70 +94,18 @@ sanitise_data_([], Protocol, Port, Instance, Sip) ->
 %%% Extra API
 %%%===================================================================
 
-%%--------------------------------------------------------------------
-%% @doc
-%% Calculates the execution time of the fun and stores it agaisnt the
-%% metric name
-%% @end
-%%--------------------------------------------------------------------
--spec(execution_time(metric(), fun()) -> any()).
-execution_time(Metric, Fun) ->
-  StartTimestamp = utc_milliseconds_time(),
-
-  Response = Fun(),
-
-  EndTimestamp = utc_milliseconds_time(),
-  update_histogram(Metric, EndTimestamp - StartTimestamp),
-  Response.
-
-%%--------------------------------------------------------------------
-%% @doc
-%% Stores a given number of updates in a histogram.
-%% @end
-%%--------------------------------------------------------------------
--spec(update_histogram(metric(), value()) -> ok).
-update_histogram(Metric, Value) when is_list(Metric), is_integer(Value) ->
-  update_or_create(get_name(Metric, histogram), histogram, Value, [{time_span, ?HISTOGRAM_TIME_SPAN}]).
-
-%%--------------------------------------------------------------------
-%% @doc
-%% Updates a counter statistic by the given value
-%% @end
-%%--------------------------------------------------------------------
--spec(update_counter(metric(), value()) -> ok).
-update_counter(Metric, Value) when is_list(Metric), is_integer(Value) ->
-  update_or_create(get_name(Metric, counter), counter, Value, []).
-
-%%--------------------------------------------------------------------
-%% @doc
-%% Updates a gauge statistic to the given value, the gauges value is
-%% always the most recent provided value.
-%% @end
-%%--------------------------------------------------------------------
--spec(update_gauge(metric(), value()) -> ok).
-update_gauge(Metric, Value) when is_list(Metric), is_integer(Value) ->
-  update_or_create(get_name(Metric, gauge), gauge, Value, []).
-
-%%--------------------------------------------------------------------
-%% @doc
-%% Updates a spiral statistic, spiral statistics maintain the total sum
-%% of all values provided within the timespan, the default is 6 seconds
-%% @end
-%%--------------------------------------------------------------------
--spec(update_spiral(metric(), value()) -> ok).
-update_spiral(Metric, Value) when is_list(Metric), is_integer(Value) ->
-  update_or_create(get_name(Metric, spiral), spiral, Value, [{time_span, ?SPIRAL_TIME_SPAN}]).
-
--spec(update_or_create(statname(), value(), type(), options()) ->ok).
-update_or_create(Name, Type, Val, Opts) ->
-  exometer:update_or_create(Name, Val, Type, Opts).
-
-%%--------------------------------------------------------------------
-%% @doc
-%% Get the name of the metric by appended the type to it, this is to
-%% distinguish with metrics of the same name.
-%% @end
-%%--------------------------------------------------------------------
--spec(get_name(metric(), type()) -> metric()).
-get_name(Metric, Type) ->
-  Metric ++ [Type].
+%%%%--------------------------------------------------------------------
+%%%% @doc
+%%%% Calculates the execution time of the fun and stores it against the
+%%%% metric name
+%%%% @end
+%%%%--------------------------------------------------------------------
+%%-spec(execution_time(metric(), fun()) -> any()).
+%%execution_time(Metric, Fun) ->
+%%  StartTimestamp = utc_milliseconds_time(),
+%%
+%%  Response = Fun(),
+%%
+%%  EndTimestamp = utc_milliseconds_time(),
+%%  update_histogram(Metric, EndTimestamp - StartTimestamp),
+%%  Response.
