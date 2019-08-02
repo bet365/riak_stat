77f8194bf05f7dbf1b3227cd1aaf65eee09b1ebc
banana <savannah.allsop@bet365.com>
2019-08-02 10:20:57 +0100

    refined @docs and -specs for the module, removed get_host and put in exoskele_console, as its function dependent and not API that encompasses exoskeleskin

--- src/exoskeleskin.erl
+++ src/exoskeleskin.erl
@@ -1,164 +1,38 @@
 %%%-------------------------------------------------------------------
 %%% @doc
-%%% todo: insert the docs from the wiki information documen
+%%% Exoskeleskin is the sub-app to riak_stat, as part of stat_management
+%%% it sets up the pushing of stats to a UDP endpoint or the body produced
+%%% when stats are requested through HTTP
+%%% Named exoskeleskin, as it only deals with the retrieval of stats on
+%%% a surface layer, no stat manipulation is made through this sub-app,
+%%% therefore it is not the bones of the stat manipulation operation
 %%% @end
 %%%-------------------------------------------------------------------
 -module(exoskeleskin).
-
 -include("exoskeleskin.hrl").
 
-%% Setup and Setdown
+%% Exports
 -export([
-    get_host/1,
     setup/1,
-    disable_port/1
+    setdown/1
 ]).
 
--spec(get_host(atom()) -> term()).
-%% @doc
-%% ETS table acts as a permanent state as the gen_servers are temporary,
-%% data is pulled out of state as a last known request
-%% @end
-get_host(Info) ->
-    case ets:lookup(?EXOSKELETABLE, Info) of
-        [{_, {MonitorServer, undefined, MonitorPort, Socket}}] ->
-            {Socket, MonitorServer, MonitorPort};
-        [{_, {_MonitorServer, MonitorServerIp, MonitorPort, Socket}}] ->
-            {Socket, MonitorServerIp, MonitorPort};
-        [] ->
-            {error, no_info}
-    end.
 
--spec(setup(term()) -> ok).
+
+-spec(setup(arg()) -> ok).
 %% @doc
-%% Set up a gen_server for udp or a gen_server to handle requests from
-%% webmachine
+%% the default operation of this function is to start up the pushing and
+%% polling of stats from exometer to the UDP endpoint "set-up".
+%% The ability to pass in an argument gives the added layer of functionality to
+%% choose the endpoint details quicker and easier, passing in arguments is optional.
 %% @end
 setup(Arg) ->
     exoskele_console:setup(Arg).
 
--spec(disable_port(term()) -> term()).
+-spec(setdown(arg()) -> ok()).
 %% @doc
-%% remove the servers currently running and pushing stats to an endpoint,
-%% removes communication between wm and client
+%% Kill the udp servers currently running and pushing stats to an endpoint.
 %% @end
-disable_port(Arg) ->
+setdown(Arg) ->
     exoskele_console:setdown(Arg).
 
-
-
-
-
-
-%%
-%%enable(udp, Por) ->
-%%    Port = exoskelestats:parse_data(Por),
-%%    case get_host() of
-%%        {error, no_udp_socket} ->
-%%            enable_udp(Port);
-%%        {_Socket, _Host, UPort} when UPort == Port ->
-%%            lager:error("UDP already enabled"),
-%%            ok;
-%%        {_Socket, _Host, _UPort} ->
-%%            change_port(udp, Port)
-%%    end;
-%%enable(http, Por) ->
-%%    Port = exoskelestats:parse_data(Por),
-%%    case get_http() of
-%%        {error, no_http_socket} ->
-%%            enable_http(Port);
-%%        {_Sock, _Host, HPort} when HPort == Port ->
-%%            lager:error("Port already open with http~n"),
-%%            ok;
-%%        {_Sock, _Host, _HPort} ->
-%%            change_port(http, Port)
-%%    end.
-%%
-%%enable_udp(Port) ->
-%%    exoskele_sup:start_server(exoskele_udp, Port).
-%%enable_http(Port) ->
-%%    exoskele_sup:start_server(exoskele_http, Port).
-%%
-%%disable(udp, _Port) ->
-%%    case get_host() of
-%%        {error, no_udp_socket} ->
-%%            ok;
-%%        {_Socket, _Host, _MPort} ->
-%%            disable_udp()
-%%    end,
-%%    ok;
-%%disable(http, _Port) ->
-%%    case get_http() of
-%%        {error, no_http_socket} ->
-%%            ok;
-%%        {_So, _In, _fo} ->
-%%            disable_http()
-%%    end.
-%%
-%%disable_udp() ->
-%%    exoskele_sup:stop_server(exoskele_udp).
-%%disable_http() ->
-%%    exoskele_sup:stop_server(exoskele_http).
-%%
-%%change_port(Arg) ->
-%%    {Type, Por} = exoskelestats:parse_arg(Arg),
-%%    Port = exoskelestats:parse_data(Por),
-%%    change_port(Type, Port).
-%%
-%%change_port(Type, Por) ->
-%%    Port = exoskelestats:parse_data(Por),
-%%      case get_host() of
-%%          {error, no_udp_socket} ->
-%%              case get_http() of
-%%                  {error, no_http_socket} ->
-%%                      case Type of
-%%                          udp ->
-%%                              enable_udp(Port);
-%%                          http ->
-%%                              enable_http(Port);
-%%                          undefined ->
-%%                              io:fwrite("error, type not defined~n")
-%%                      end;
-%%                  _ ->
-%%                      case Type of
-%%                          udp ->
-%%                              disable_http(),
-%%                              enable_udp(Port);
-%%                          http ->
-%%                              restart(http, Port);
-%%                          undefined ->
-%%                              restart(http, Port)
-%%                      end
-%%              end;
-%%            _ ->
-%%              case Type of
-%%                  udp ->
-%%                      enable_udp(Port);
-%%                  http ->
-%%                      disable_udp(),
-%%                      enable_http(Port);
-%%                  undefined ->
-%%                      restart(udp, Port)
-%%              end
-%%    end.
-%%
-%%restart(udp, Por) ->
-%%    Port = exoskelestats:parse_data(Por),
-%%    disable_udp(),
-%%    enable_udp(Port),
-%%    lager:info("Port for UDP=~p~n", [Port]);
-%%restart(http, Por) ->
-%%    Port = exoskelestats:parse_data(Por),
-%%    disable_http(),
-%%    enable_http(Port),
-%%    lager:info("Port for HTTP=~p~n", [Port]).
-%%
-%%restart(udp) ->
-%%    restart(udp, 0);
-%%restart(http) ->
-%%    restart(http, 0);
-%%restart(Arg)->
-%%    {Type, Port} = exoskelestats:parse_arg(Arg),
-%%    restart(Type, Port).
-%%
-%%
