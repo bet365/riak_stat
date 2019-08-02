5455f3fc1e0cc33560553888ddb2a2af2fee8d89
banana <savannah.allsop@bet365.com>
2019-08-02 11:13:24 +0100

    moved get_host/0 to this module as it is a function call from other modules and not a top layer function

--- src/exoskele_console.erl
+++ src/exoskele_console.erl
@@ -1,55 +1,84 @@
 %%%-------------------------------------------------------------------
 %%% @doc
-%%% console arguments are sent to this module to sanitise data and to
-%%% start/terminate gen_servers
+%%% the hub of sanitising Arugments passed into exoskeleskin and sending
+%%% data received from console to where it needs to go, retrieving data
+%%% for udp and/or http functions as well a function calls to riak_stat
+%%% to poll stats.
 %%% @end
 %%%-------------------------------------------------------------------
 -module(exoskele_console).
+-include("exoskeleskin.hrl").
 
 -export([
     setup/1,
-    setdown/1
+    setdown/0,
+    setdown/1,
+    get_host/1
 ]).
 
-%%%===================================================================
-%%% Internal functions
-%%%===================================================================
-start_http() ->
-  Dispatch = cowboy_router:compile([
-    {'_', [{"/stats/",      exo_stats_handler, []}]}
-  ]),
+-type hostarg()     :: atom().
 
-  {ok, _} = cowboy:start_http(exo_http_listener, 100, [{host, {0,0,0,0}}, {port, ?STATS_LISTEN_PORT}],
-    [{env, [{dispatch, Dispatch}]}]
-  ).
-
-
-
+%%-------------------------------------------------------------------
+-spec(setup(arg()) -> ok).
+%% @doc
+%% sanitise the argument passed in to retrieve the data from console,
+%% passing in information is optional as all the defaults are pulled out from
+%% the sys.config.
+%% similar to other stats functions the stats can be passed in to only poll
+%% those stats, however, only the enabled stats will return with values
+%%
+%% If the server is already setup and polling stats, setting up another will
+%% "restart" the gen_server.
+%% @end
 setup(Arg) ->
-    {{Protocol, Port, Instance, Sip}, Stats} = sanitise_data(Arg),
-    Name = protocol_name(Protocol),
-    start_server(Name, {{Port, Instance, Sip}, Stats}).
+      {{Port, Instance, Sip}, STATSorPROFILES}    =     sanitise_data(Arg),
+      start_server(exoskele_udp, {{Port, Instance, Sip}, STATSorPROFILES}).
 
-sanitise_data(Arg) ->
-    exoskele_data:sanitise_data(Arg).
-
-protocol_name(Protocol) ->
-    Proto = re:split([<<"exoskele_">>|[Protocol]], "\\."),
-    binary_to_atom(Proto, latin1).
+sanitise_data([<<>>]) -> sanitise_data([]);
+sanitise_data(Arg) -> exoskele_data:sanitise_data(Arg).
 
 start_server(Child, Arg) ->
-    exoskele_sup:start_server(Child, Arg).
+    case get_child() of
+        {error, Reason} ->
+            lager:error("Couldn't find Children because: ~p~n", [Reason]);
+        ChildRef ->
+            Ids = [Id || {Id, _Child, _Type, _Mods} <- ChildRef],
+            terminate_server(Ids),
+            exoskele_sup:start_server(Child, Arg)
+    end.
 
-setdown([]) ->
-    Children = get_child(),
-    terminate_server(Children);
-setdown(Arg) ->
-    {Protocol, _Port, _Instance, _Sip} = sanitise_data(Arg),
-    Name = protocol_name(Protocol),
-    terminate_server(Name).
+
+%%-------------------------------------------------------------------
+-spec(setdown(arg()) -> ok).
+%% @doc
+%% Stop the pushing of stats by taking the setup of the udp gen_server
+%% down
+%% @end
+setdown() ->
+    setdown([]).
+setdown(_Arg) ->
+    terminate_server(get_child()).
 
 get_child() ->
-  exoskele_sup:what_kids().
+    exoskele_sup:what_kids().
 
 terminate_server(Child) ->
     exoskele_sup:terminate(Child).
+
+
+%%-------------------------------------------------------------------
+-spec(get_host(hostarg()) -> {socket(), server_ip() | server(), port()}).
+%% @doc
+%% get the host details of the udp_socket or http request details, similar to the state
+%% in a gen_server but kept in an ets - table to preserve it longer that the udp
+%% gen_server, information is pulled out like a last known request
+%% @end
+get_host(Info) ->
+    case ets:lookup(?EXOSKELETABLE, Info) of
+        [{_, {MonitorServer, undefined, MonitorPort, Socket}}] ->
+            {Socket, MonitorServer, MonitorPort};
+        [{_, {_MonitorServer, MonitorServerIp, MonitorPort, Socket}}] ->
+            {Socket, MonitorServerIp, MonitorPort};
+        [] ->
+            {error, no_info}
+    end.
