4dc1fa5a0c92e036efb6b9cf1824ad64777cf64c
banana <savannah.allsop@bet365.com>
2019-08-08 11:45:48 +0100

    added branch of exometer_core to rebar config

--- src/riak_stat_app.erl
+++ src/riak_stat_app.erl
@@ -13,8 +13,8 @@ start(StartType, StartArgs) ->
     io:format("riak_stat_app:start(~p,~p)~n", [StartType, StartArgs]),
     riak_stat_sup:start_link(),
     io:format("riak_stat_sup:start_link()"),
-    Started = riak_stat_config:ensure_all(),
-    io:format("riak_stat_config:ensure_all()~nStarted: ~p~n", [Started]),
+%%    Started = riak_stat_config:ensure_all(),
+%%    io:format("riak_stat_config:ensure_all()~nStarted: ~p~n", [Started]),
     exoskele_sup:start_link(),
     io:format("exoskele_sup:start_link()").
 
